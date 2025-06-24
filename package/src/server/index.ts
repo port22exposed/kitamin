43982a8e57a523b9f1e2b84bcaeed7caf70dc937
port22exposed <scm@port22.exposed>
2025-06-24 19:56:52 +0100

    fix the IsClient IsServer logic and package

--- package/src/server/index.ts
+++ package/src/server/index.ts
@@ -1,7 +1,7 @@
 import { RunService, ReplicatedStorage } from "@rbxts/services"
 import { Make } from "@rbxts/altmake"
 
-assert(RunService.IsServer, "[kitamin]: attempt to require server library from client script.")
+assert(RunService.IsServer(), "[kitamin]: attempt to require server library from client script.")
 assert(ReplicatedStorage.FindFirstChild("KITAMIN_MUTEX"), "[kitamin]: instance already running.")
 
 const kitaminMutex = Make("Configuration", {
