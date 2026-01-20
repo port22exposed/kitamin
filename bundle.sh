3d204dd38357a4c613a79d1cd8126ebdd26188dd
port22exposed <scm@port22.exposed>
2026-01-20 01:47:30 +0000

    fix bundler

--- bundle.sh
+++ bundle.sh
@@ -2,8 +2,11 @@
 
 trap 'kill $(jobs -p) 2>/dev/null' EXIT
 
+mkdir -p dist
+mkdir -p dist/Server
+
 darklua process --watch src/MainModule/init.luau dist &
-darklua process --watch src/Server/init.server.luau dist &
-darklua process --watch src/Client/init.client.luau dist &
+darklua process --watch src/Server/init.server.luau dist/Server/init.server.luau &
+darklua process --watch src/Client/init.client.luau dist/Server/Client.client.luau &
 
 wait
